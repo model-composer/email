c22fb477962e8ca054fbae8e1cb6e22bddd59987
Domenico Giambra <zorinik@gmail.com>
2022-05-16 23:35:17 +0200

    Config version upgrade

--- src/Email.php
+++ src/Email.php
@@ -11,25 +11,7 @@ class Email
 
 	public function __construct(string $subject, string $text, array $options = [])
 	{
-		$config = Config::get('email', function () {
-			return [
-				'from' => [
-					'mail' => '',
-					'name' => defined('APP_NAME') ? APP_NAME : '',
-				],
-				'smtp' => false,
-				'port' => 25,
-				'header' => '<div style="width: 800px; margin: auto"><p style="text-align: center"><img src="https://' . $_SERVER['HTTP_HOST'] . (defined('PATH') ? PATH : '/') . 'app/assets/img/logo.png" alt="" /></p>',
-				'footer' => '</div>',
-				'debug' => false,
-				'username' => null,
-				'password' => null,
-				'encryption' => null,
-				'charset' => 'UTF-8',
-			];
-		});
-
-		$this->options = array_merge($config, $options);
+		$this->options = array_merge($this->getConfig(), $options);
 
 		// Backward compatibility
 		if (isset($this->options['from_mail'])) {
@@ -113,5 +95,44 @@ class Email
 
 		if ($this->options['smtp'])
 			$this->message->SmtpClose();
+	}
+
+	private function getConfig(): array
+	{
+		return Config::get('email', [
+			'version' => '0.2.0',
+			'migration' => function () {
+				if (defined('INCLUDE_PATH') and file_exists(INCLUDE_PATH . 'app/config/Email/config.php')) {
+					// ModEl 3 migration
+					require(INCLUDE_PATH . 'app/config/Email/config.php');
+
+					$config['from'] = [
+						'mail' => $config['from_mail'],
+						'name' => $config['from_name'],
+					];
+
+					unset($config['from_mail']);
+					unset($config['from_name']);
+
+					return $config;
+				}
+
+				return [
+					'from' => [
+						'mail' => '',
+						'name' => defined('APP_NAME') ? APP_NAME : '',
+					],
+					'smtp' => false,
+					'port' => 25,
+					'header' => '<div style="width: 800px; margin: auto"><p style="text-align: center"><img src="https://' . $_SERVER['HTTP_HOST'] . (defined('PATH') ? PATH : '/') . 'app/assets/img/logo.png" alt="" /></p>',
+					'footer' => '</div>',
+					'debug' => false,
+					'username' => null,
+					'password' => null,
+					'encryption' => null,
+					'charset' => 'UTF-8',
+				];
+			},
+		]);
 	}
 }
