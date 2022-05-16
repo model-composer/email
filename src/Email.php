07792effe33024425a338abcccede7bb2b5e6f45
Domenico Giambra <zorinik@gmail.com>
2022-05-16 23:36:37 +0200

    Migration fix

--- src/Email.php
+++ src/Email.php
@@ -100,39 +100,41 @@ class Email
 	private function getConfig(): array
 	{
 		return Config::get('email', [
-			'version' => '0.2.0',
-			'migration' => function () {
-				if (defined('INCLUDE_PATH') and file_exists(INCLUDE_PATH . 'app/config/Email/config.php')) {
-					// ModEl 3 migration
-					require(INCLUDE_PATH . 'app/config/Email/config.php');
+			[
+				'version' => '0.2.0',
+				'migration' => function () {
+					if (defined('INCLUDE_PATH') and file_exists(INCLUDE_PATH . 'app/config/Email/config.php')) {
+						// ModEl 3 migration
+						require(INCLUDE_PATH . 'app/config/Email/config.php');
 
-					$config['from'] = [
-						'mail' => $config['from_mail'],
-						'name' => $config['from_name'],
+						$config['from'] = [
+							'mail' => $config['from_mail'],
+							'name' => $config['from_name'],
+						];
+
+						unset($config['from_mail']);
+						unset($config['from_name']);
+
+						return $config;
+					}
+
+					return [
+						'from' => [
+							'mail' => '',
+							'name' => defined('APP_NAME') ? APP_NAME : '',
+						],
+						'smtp' => false,
+						'port' => 25,
+						'header' => '<div style="width: 800px; margin: auto"><p style="text-align: center"><img src="https://' . $_SERVER['HTTP_HOST'] . (defined('PATH') ? PATH : '/') . 'app/assets/img/logo.png" alt="" /></p>',
+						'footer' => '</div>',
+						'debug' => false,
+						'username' => null,
+						'password' => null,
+						'encryption' => null,
+						'charset' => 'UTF-8',
 					];
-
-					unset($config['from_mail']);
-					unset($config['from_name']);
-
-					return $config;
-				}
-
-				return [
-					'from' => [
-						'mail' => '',
-						'name' => defined('APP_NAME') ? APP_NAME : '',
-					],
-					'smtp' => false,
-					'port' => 25,
-					'header' => '<div style="width: 800px; margin: auto"><p style="text-align: center"><img src="https://' . $_SERVER['HTTP_HOST'] . (defined('PATH') ? PATH : '/') . 'app/assets/img/logo.png" alt="" /></p>',
-					'footer' => '</div>',
-					'debug' => false,
-					'username' => null,
-					'password' => null,
-					'encryption' => null,
-					'charset' => 'UTF-8',
-				];
-			},
+				},
+			],
 		]);
 	}
 }
